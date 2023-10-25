83a7620a819795c82f21c38da24ecfce7e0d8be8
Gunf1ghterr <130758382+Gunf1ghterr@users.noreply.github.com>
2023-10-25 20:10:09 +0300

    add two pages(Policy and Agreement))

--- Backend/frontend/src/components/App.tsx
+++ Backend/frontend/src/components/App.tsx
@@ -4,6 +4,8 @@ import Header from "./Header";
 import Navbar from "./Navbar";
 import HomePage from "./pages/HomePage";
 import Payments from "./pages/Payments";
+import Agreement from "./pages/UserAgreement"
+import Policy from "./pages/Policy"
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
       <Routes>
         <Route element={<HomePage/>} path="" />
         <Route element={<Payments/>} path="/payments" />
+        <Route element={<Agreement/>} path="/agreement"/>
+        <Route element={<Policy/>} path="/privacy-policy"/>
       </Routes>
       <Footer />
     </BrowserRouter>
